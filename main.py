af2d8e3e58bf28b2e3e0140bc775be6ac317913f
nautrw <nautrw@proton.me>
2026-03-12 12:47:48 -0400

    add function to quantify observations

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ data = pd.read_csv("data.csv")
 
 def exclude_spore_colonies(data):
     return data.set_index("Observation").drop("Spore Colonies").reset_index()
+
+
+def quantify_observations(data):
+    values = {"None": 0, "Below Average": 1, "Average": 2, "Above Average": 3}
+
+    data["Observation"] = data["Observation"].replace(values)
+
+    return data
