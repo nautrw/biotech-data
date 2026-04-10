0ec6f46805f138d163ef6be31659b920676e8492
nautrw <nautrw@proton.me>
2026-04-10 11:09:18 -0400

    reduce easter egg chance

--- biotech_data/easter_egg.py
+++ biotech_data/easter_egg.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 def do_easter_egg():
-    if random.random() < 0.51:
+    if random.random() < 0.01:
         eggs = [st.balloons, st.snow]
 
         random.choice(eggs)()
