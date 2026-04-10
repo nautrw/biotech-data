20d24d195fd375e16609f6e5ed8eb903a960e2db
nautrw <nautrw@proton.me>
2026-04-10 12:15:20 -0400

    print time on easter egg

--- biotech_data/easter_egg.py
+++ biotech_data/easter_egg.py
@@ -1,11 +1,14 @@
 import random
+from datetime import datetime
 
 import streamlit as st
 
 
 def do_easter_egg():
     if random.random() < 0.01:
+        now = datetime.now()
         eggs = [st.balloons, st.snow]
 
         random.choice(eggs)()
         st.toast("You found an easter egg!", icon="🥚", duration="long")
+        print(f"An easter egg was found at {now.strftime('%Y-%m-%d %H:%M:%S')}")
